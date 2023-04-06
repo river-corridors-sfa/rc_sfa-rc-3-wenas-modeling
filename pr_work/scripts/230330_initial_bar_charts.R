fa758641e9e711a7849824a314115be1ac6b85d5
peterregier <peter.regier@pnnl.gov>
2023-04-06 08:44:37 -0600

    add initial bar charts

--- pr_work/scripts/230330_initial_bar_charts.R
+++ pr_work/scripts/230330_initial_bar_charts.R
@@ -1,7 +1,7 @@
 ## This script reads in model results and starts creating some plots that might be
 ## useful.
 ##
-## pjr, 2/30/23
+## pjr, 3/30/23
 ##
 ###############
 
@@ -48,26 +48,40 @@ read_data <- function(file){
     pivot_wider(values_from = c("flow", "sed", "nitrate", "doc"), names_from = "metric")
 }
 
-df_raw <- files %>% 
+## Create raw dataset for fire scenarios
+fires_raw <- files %>% 
   filter(grepl("PER_", name)) %>% 
   pull(name) %>% 
   map(read_data) %>% 
   bind_rows() %>% 
   distinct()
 
-df_base <- df_raw %>% 
+## Create raw dataset for watershed scenarios
+ws_raw <- files %>% 
+  filter(!grepl("PER_", name)) %>% 
+  filter(!grepl("old_swat", name)) %>% #manually remove problem file
+  filter(!grepl("nofire|unburned", name)) %>% # remove nofire: not needed and not formatted correctly
+  pull(name) %>% 
+  map(read_data) %>% 
+  bind_rows() %>% 
+  distinct()
+
+fire_base <- fires_raw %>% 
   filter(scenario == "base_unburned")
 
 fire_colors = c("#F5C63C", "#F47F6B", "#BB5098")
 
-df_fires <- df_raw %>% 
+df_fires <- fires_raw %>% 
   filter(scenario != "base_unburned") %>% 
   separate(scenario, c("x", "percent", "scenario"), sep = "_") %>% 
   mutate(percent = fct_relevel(as.factor(percent), c("10", "15", "20", "25", "30", "40", "45", "50", "60", "75", "90", "100")), 
          scenario = fct_relevel(as.factor(scenario), c("LOW", "MOD", "HIGH"))) %>% 
   select(-x)
 
-make_geom_col <- function(variable_of_interest){
+
+# 3. Make fire plots -----------------------------------------------------------
+
+make_geom_col_fire <- function(variable_of_interest){
    
   var <- paste0(variable_of_interest, "_mean")
   
@@ -76,15 +90,15 @@ make_geom_col <- function(variable_of_interest){
   
   ggplot(x, aes_string(x = "percent", y = var, fill = "scenario")) +
      geom_col(position = "dodge", color = "black", alpha = 0.5) + 
-    geom_hline(yintercept = df_base$flow_mean) +
+    geom_hline(yintercept = fire_base$flow_mean) +
     scale_fill_manual(values = fire_colors)
 
 }
 
-plot_grid(make_geom_col("flow") + ylab("Flow (m3/s)"),
-          make_geom_col("sed") + ylab("Sediment"), 
-          make_geom_col("nitrate") + ylab("Nitrate"), 
-          make_geom_col("doc") + ylab("DOC (mg/L)"), 
+plot_grid(make_geom_col_fire("flow") + ylab("Flow (m3/s)"),
+          make_geom_col_fire("sed") + ylab("Sediment"), 
+          make_geom_col_fire("nitrate") + ylab("Nitrate"), 
+          make_geom_col_fire("doc") + ylab("DOC (mg/L)"), 
           ncol = 1)
 ggsave("figures/230330_fire_characteristics_bar_charts.png", width = 6, height = 12)
 
@@ -95,7 +109,39 @@ flow <- ggplot(df_fires, aes(x = percent, fill = scenario)) +
   scale_fill_manual(values = fire_colors)
 
 
+# 4. Make watershed characteristics plots --------------------------------------
 
+## First, set up classifications
+df_ws <- ws_raw %>% 
+  separate(scenario, c("ID", "scenario"), sep = "_", extra = "merge") %>% 
+  separate(scenario, c("scenario", "SLP"), sep = "_", extra = "merge")
+
+## Next, set up the plotting function
+make_geom_col_ws <- function(variable_of_interest){
+  
+  var <- paste0(variable_of_interest, "_mean")
+  
+  x <- df_ws %>% 
+    select(scenario, percent, var)
+  
+  ggplot(x, aes_string(x = "percent", y = var, fill = "scenario")) +
+    geom_col(position = "dodge", color = "black", alpha = 0.5) + 
+    geom_hline(yintercept = fire_base$flow_mean) +
+    scale_fill_manual(values = fire_colors)
+  
+}
+
+
+colnames(df_ws)
+
+## For SLP, because names have different #s of things, we need to get creative
+df_ws %>% filter(scenario == "SLP") %>% 
+  mutate(sign = ifelse(grepl("neg", SLP), "neg", "pos")) %>% # Create a col for sign
+  mutate(SLP = str_remove(SLP, "neg_")) %>% 
+  separate(SLP, c("slope", "fire"), sep = "_")
+
+
+ggplot(df_ws, aes(ID, ))
 
 
 
