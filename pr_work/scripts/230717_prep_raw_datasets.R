e3ae80dd2c7495e5c6ea735b704a721dab70a426
peterregier <peter.regier@pnnl.gov>
2023-07-18 17:00:52 -0600

    upload rmd to make dataset
    ignore the rmd, it's ugggglly

--- pr_work/scripts/230717_prep_raw_datasets.R
+++ pr_work/scripts/230717_prep_raw_datasets.R
@@ -69,7 +69,79 @@ download_data(results_files, results_path)
 download_data(summary_files, summary_path)
 
 
-# 3. Read in 
+# 3. Read in results files -----------------------------------------------------
+
+## Make a function to read in data and clean uop filenames
+read_results <- function(file){
+  read_csv(paste0(results_path, file)) %>% 
+    mutate(file = str_remove(file, "_fire_results.csv")) %>% 
+    mutate(file = str_remove(file, "_results.csv")) %>% 
+    mutate(file = str_remove(file, "PER_")) %>% 
+    mutate(file = str_remove(file, "HRU_SLP_")) %>% 
+    clean_names() %>% 
+    select(dates, file, contains("_fire"), contains("_nofire")) %>% 
+    select(!contains("_kgday"))
+}
+
+## Read the data in 
+results_raw <- results_files$name %>% 
+  map(read_results) %>% 
+  bind_rows()
+
+## Clean up and categorize the files -------------------------------------------
+
+file_values_to_scrub = c("base_burned", "LOW", "MOD", "HIGH")
+
+percent_change = function(var, baseline){
+  (({{var}} - {{baseline}}) / {{baseline}}) * 100
+}
+
+results_data <- results_raw %>% 
+  filter(dates > "2019-12-31") %>% 
+  filter(!(file %in% file_values_to_scrub)) %>% 
+  mutate(file = case_when(file == "1" ~ "pos_1", 
+                          file == "0.1" ~ "pos_0.1",
+                          file == "0.5" ~ "pos_0.5",
+                          TRUE ~ file)) %>% 
+  mutate(category = case_when(grepl("LOW", file) | grepl("MOD", file) | grepl("HIGH", file) ~ "severity", 
+                              file == "WET" | file == "DRY" ~ "precipitation", 
+                              file == "GWQ" | file == "LATQ" | file == "SURQ" ~ "flowpath",
+                              file == "deciduous" | file == "shrub" | file == "coniferous" | file == "grass" ~ "vegetation",
+                              file == "base_burned" ~ "base",
+                              TRUE ~ "slope")) %>% 
+  mutate(flow_perc = percent_change(flow_m3s_fire, flow_m3s_nofire), 
+         sed_perc = percent_change(sed_mg_l_fire, sed_mg_l_nofire), 
+         nitrate_perc = percent_change(nit_mg_l_fire, nit_mg_l_nofire), 
+         doc_perc = percent_change(doc_mg_l_fire, doc_mg_l_nofire))
+
+median_ = function(...){median(..., na.rm = T)}
+mean_ = function(...){mean(..., na.rm = T)}
+
+results_stats <- results_data %>% 
+  group_by(file) %>% 
+  summarize(category = first(category), 
+            flow_median = median_(flow_perc), 
+            sed_median = median_(sed_perc), 
+            nitrate_median = median_(nitrate_perc), 
+            doc_median = median_(doc_perc))
+
+severity_stats <- results_stats %>% 
+  filter(category == "severity") %>%
+  separate(file, into = c("percent", "severity")) %>% 
+  mutate(percent = as.factor(as.numeric(percent))) %>% #simple way to reorder as a factor
+  mutate(severity = fct_relevel(severity, c("LOW", "MOD", "HIGH")))
+
+ggplot(severity_stats, aes(percent, flow_median, fill = severity)) + 
+  geom_col()
+
+
+  facet_wrap(~category, nrow = 1, scales = "free") + 
+  scale_x_continuous(trans = pseudolog10_trans)
+
+
+
+
+
 
 
 
